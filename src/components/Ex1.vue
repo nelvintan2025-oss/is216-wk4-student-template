2e59708747c51c4bcb65a36d3e0e5b7aad90194f
nelvintan2025-oss <nelvin.tan.2025@smu.edu.sg>
2026-09-06 22:22:29 +0800

    Completed Ex1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,18 +1,19 @@
 <script setup>
-    import { ref } from 'vue'
+import { ref } from 'vue'
 
-    // Add code here
-    // challenge: use array  
-    
+const message = ref("Choose your favourite fruit")
+
+// Challenge: Single array storing all fruits
+const fruits = ref(["apple", "orange", "peach"])
 </script>
 
 <template>
-    {{ message }} 
-   
-    <label for=""> <input type="radio" name="fruit"> {{ fruit1 }} </label> 
-    <label for=""> <input type="radio" name="fruit"> {{ fruit2 }} </label> 
-    <label for=""><input type="radio" name="fruit"> {{ fruit3 }} </label> 
-    
+  {{ message }}
+
+  <!-- Loop through the array directly -->
+  <label v-for="fruit in fruits" :key="fruit">
+    <input type="radio" name="fruit"> {{ fruit }}
+  </label>
 </template>
 
 <style scoped>
